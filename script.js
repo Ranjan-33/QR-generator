43693d5902e77647f61108c05e9925cfe924e41a
Ranjan <vermaranjan9964@gmail.com>
2023-08-31 01:10:15 +0530

    QR generator working fine

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
+let imgBox = document.getElementById("imgBox");
+let QRimage = document.getElementById("QRimage");
+let qrText = document.getElementById("qrText");
 
-let imgBox=document.getElementById("imgBox");
-let QRimage=document.getElementById("QRimage");
-let qrText=document.getElementById("qrText");
+function generateQR() {
+    
+  if (qrText.value.length > 0) {
+    QRimage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
+    imgBox.classList.add("show-img");
+  } else {
 
-function generateQR(){
-    if(qrText.value.length>0){
-        QRimage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +qrText.value;
-        imgBox.classList.add("show-img")
-    }
-
-    else{
-     
-        qrText.classList.add('error')
-        setInterval(()=>{
-            qrText.classList.remove('error')
-        },1000)
-        // alert("plese insert the text or URL")
-    }
-
+    qrText.classList.add("error");
+    setInterval(() => {
+      qrText.classList.remove("error");
+    }, 1000);
+    // alert("plese insert the text or URL")
+  }
 }
