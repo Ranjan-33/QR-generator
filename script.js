5f08ff3ac0786ee5dbe036be5674661070cb222f
Ranjan <vermaranjan9964@gmail.com>
2023-09-04 11:09:18 +0530

    updated Enter Key

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-let imgBox = document.getElementById("imgBox");
-let QRimage = document.getElementById("QRimage");
-let qrText = document.getElementById("qrText");
+document.addEventListener('DOMContentLoaded', function () {
+  let imgBox = document.getElementById("imgBox");
+  let QRimage = document.getElementById("QRimage");
+  let qrText = document.getElementById("qrText");
 
-function generateQR() {
-    
-  if (qrText.value.length > 0) {
-    QRimage.src =
-      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-      qrText.value;
-    imgBox.classList.add("show-img");
-  } else {
-
-    qrText.classList.add("error");
-    setInterval(() => {
-      qrText.classList.remove("error");
-    }, 1000);
-    // alert("plese insert the text or URL")
+  function generateQR() {
+    if (qrText.value.length > 0) {
+      QRimage.src =
+        "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+        qrText.value;
+      imgBox.classList.add("show-img");
+    } else {
+      qrText.classList.add("error");
+      setTimeout(() => {
+        qrText.classList.remove("error");
+      }, 1000);
+    }
   }
-}
+
+  const qrForm = document.getElementById('qrForm');
+
+  qrForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    generateQR();
+  });
+
+  qrText.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      generateQR();
+    }
+  });
+});
